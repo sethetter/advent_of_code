b963b50d3baa45a344cb79ed5a94fc98bd12c803
Seth Etter <sethetter@gmail.com>
2015-12-04 21:56:09 -0600

    Second part of day 1, done

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -3,13 +3,30 @@ defmodule AdventOfCode.Day1 do
     floor_count(String.codepoints(input), 0)
   end
 
-  defp floor_count([hd | tl], count) do
+  def first_time_to_basement(input) do
+    first_time_to_basement(String.codepoints(input), 0, 1)
+  end
+
+  # Private
+
+  defp floor_count([hd | tl], floor) do
     cond do
-      hd == "(" -> floor_count(tl, count + 1)
-      hd == ")" -> floor_count(tl, count - 1)
-      true -> floor_count(tl, count)
+      hd == "(" -> floor_count(tl, floor + 1)
+      hd == ")" -> floor_count(tl, floor - 1)
+      true -> floor_count(tl, floor)
     end
   end
 
+  defp floor_count([], floor), do: floor
+
   defp floor_count([], count), do: count
+
+  defp first_time_to_basement([hd | tl], floor, index) do
+    cond do
+      floor == -1 -> index + 1
+      hd == "(" -> first_time_to_basement(tl, floor + 1, index + 1)
+      hd == ")" -> first_time_to_basement(tl, floor - 1, index + 1)
+      true -> first_time_to_basement(tl, floor, index + 1)
+    end
+  end
 end
--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -2,15 +2,21 @@ defmodule AdventOfCode.Day1Test do
   use ExUnit.Case
   doctest AdventOfCode.Day1
 
-  test "counts ( as going up a floor" do
+  test "#floor_count :: counts ( as going up a floor" do
     {_, input} = File.read("test/day_1_input_1.txt")
     floor = AdventOfCode.Day1.floor_count(input)
     assert(floor == 4)
   end
 
-  test "counts ) as going down a floor" do
+  test "#floor_count :: counts ) as going down a floor" do
     {_, input} = File.read("test/day_1_input_2.txt")
     floor = AdventOfCode.Day1.floor_count(input)
     assert(floor == -5)
+  end
+
+  test "#first_time_to_basement :: returns current index once floor is -1" do
+    {_, input} = File.read("test/day_1_input_2.txt")
+    in_basement_at = AdventOfCode.Day1.first_time_to_basement(input)
+    assert(in_basement_at == 3)
   end
 end
