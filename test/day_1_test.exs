ca6b0159e0fc867cd23e0cf0ae40881c86eac8a1
Seth Etter <sethetter@gmail.com>
2015-12-04 22:17:54 -0600

    Moved these, woops

--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day1Test do
   doctest AdventOfCode.Day1
 
   test "#floor_count :: counts ( as going up a floor" do
-    {_, input} = File.read("test/day_1_input_1.txt")
+    {_, input} = File.read("test/input/day_1_input_1.txt")
     floor = AdventOfCode.Day1.floor_count(input)
     assert(floor == 4)
   end
 
   test "#floor_count :: counts ) as going down a floor" do
-    {_, input} = File.read("test/day_1_input_2.txt")
+    {_, input} = File.read("test/input/day_1_input_2.txt")
     floor = AdventOfCode.Day1.floor_count(input)
     assert(floor == -5)
   end
 
   test "#first_time_to_basement :: returns current index once floor is -1" do
-    {_, input} = File.read("test/day_1_input_3.txt")
+    {_, input} = File.read("test/input/day_1_input_3.txt")
     in_basement_at = AdventOfCode.Day1.first_time_to_basement(input)
     assert(in_basement_at == 3)
   end
